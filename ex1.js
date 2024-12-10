ad8595fc6eefb10a90394cc53a00a32a2f8140d5
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2024-12-11 00:22:52 +0530

    step 6 done
    wrote the logic to play the game 5 times and snd show the results of best of 5.

--- ex1.js
+++ ex1.js
@@ -27,11 +27,35 @@ function playRound(humanChoice, computerChoice){
     }
     else if(humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper"){
         alert("Player wins");
+        humanScore++;
         console.log(`So far the score is Player: ${humanScore} , Computer: ${computerScore} .`);
+        
     }
     else{
         alert("Computer wins");
+        computerScore++;
         console.log(`So far the score is Player: ${humanScore} , Computer: ${computerScore} .`);
     }
 
 }
+
+function playGame(){
+    for(let count = 1; count < 6 ; count++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    if(humanScore == computerScore){
+        alert("The Scores are Tied");
+        console.log(`The Final Score is Player: ${humanScore} , Computer: ${computerScore} . `);
+    }
+    else if(humanScore > computerScore){
+        alert("Player Wins");
+        console.log(`The Final Score is Player: ${humanScore} , Computer: ${computerScore} . `);
+    }
+    else{
+        alert("Computer Wins");
+        console.log(`The Final Score is Player: ${humanScore} , Computer: ${computerScore} . `);
+    }
+}
+playGame();
