c22a0e3723b42d7a1294a1c54d0f2605f9c6ed3f
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2024-12-10 23:57:39 +0530

    started building a rock paper scissors game
    following the instructions of the odin project step 1 and 2 done

---
+++ ex1.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()*3)
+    if(computerChoice == 0){
+        computerChoice = "rock";
+    }
+    else if(computerChoice == 1){
+        computerChoice = "paper";
+    }
+    else{
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
